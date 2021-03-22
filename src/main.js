26a5de69e2b9cf9fa6876337b5786c06a73c5f50
Vaneluuq <76055965+Vaneluuq@users.noreply.github.com>
2021-03-22 11:43:33 +0100

    se añadio estructura HTML

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { example } from './data.js';
+// import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+// import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+// console.log(example, data);
 
-console.log(example, data);
+let imagenes = ["imagenes/imag1.jpg","imagenes/imag2.jpg","imagenes/imag3.jpg","imagenes/imag4.jpg"];
+let contador = 0;
+
+function carrusel(contenedorImagenes) {
+ contenedorImagenes.addeventlistener("click", e => {
+   const atras = contenedorImagenes.querySelector(".btnAtras");
+   const adelante = contenedorImagenes.querySelector(".btnAdelante");
+   const img = contenedorImagenes.querySelector(".corruselImagen");
+   let tgt = e.target;
+   
+   if(tgt==atras) {
+      if(contador > 0) {
+        img.src = imagenes [contador-1];
+        contador--;
+      }else {
+        img.src = imagenes [imagenes.length-1];
+      }
+   } else if() {
+
+   }
+ });
+}
