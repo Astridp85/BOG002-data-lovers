95a7e2b2911d96816d23b544881df495b2c7a9a5
Astrid Pinero <astrid.pinero85@gmail.com>
2021-03-22 19:32:16 -0500

    Se incluye footer con su estilo. slice de fotos mas populares

--- src/main.js
+++ src/main.js
@@ -4,25 +4,4 @@
 // import data from './data/rickandmorty/rickandmorty.js';
 // console.log(example, data);
 
-let imagenes = ["imagenes/imag1.jpg","imagenes/imag2.jpg","imagenes/imag3.jpg","imagenes/imag4.jpg"];
-let contador = 0;
 
-function carrusel(contenedorImagenes) {
- contenedorImagenes.addeventlistener("click", e => {
-   const atras = contenedorImagenes.querySelector(".btnAtras");
-   const adelante = contenedorImagenes.querySelector(".btnAdelante");
-   const img = contenedorImagenes.querySelector(".corruselImagen");
-   let tgt = e.target;
-   
-   if(tgt==atras) {
-      if(contador > 0) {
-        img.src = imagenes [contador-1];
-        contador--;
-      }else {
-        img.src = imagenes [imagenes.length-1];
-      }
-   } else if() {
-
-   }
- });
-}
