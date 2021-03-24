7c2a629d0248d3c7bd16cfb23ac15dbbb793bbfb
Vaneluuq <76055965+Vaneluuq@users.noreply.github.com>
2021-03-24 17:20:44 +0100

    Se añadio header y main responsive

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
-// import { example } from './data.js';
-// import data from './data/lol/lol.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// console.log(example, data);
+import data from './data/athletes/athletes.js';
+
+let atletas = data.athletes;
+let nombresContenido = ""
+
+for (let i=0; i<atletas.length; i++){
+let nombreAtleta = atletas[i].name;
+nombresContenido = nombresContenido + nombreAtleta
+}
 
 
+// evento menu responsive 
+let div = document.getElementById("menuToggle");
+div.addEventListener("click", mostrar)
+function mostrar() {
+    let abrirNavegacion = document.getElementById("opcionesMenu");
+    abrirNavegacion.classList.toggle("opcionesMenuOpen")
+
+    let menuOpen = document.getElementById('menuToggle');
+    menuOpen.classList.toggle('menuToggleOpen');    
+}
+  
+
+    
