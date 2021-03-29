01f24b24eb54828b1535c4ff88c790a6b8d0b38b
Astrid Pinero <astrid.pinero85@gmail.com>
2021-03-29 17:22:48 -0500

    tarjetas mostradas desde JS popup animados, footer arreglado, confiable y todo funcionando hasta el momento

--- src/main.js
+++ src/main.js
@@ -1,73 +1,70 @@
 import data from './data/athletes/athletes.js';
-
-// let atletas = data.athletes;
-// let nombresContenido = ""
-// let atletasArray = []
-
-// for (let i=0; i<atletas.length; i++){
-// let nombreAtleta = atletas[i].name;
-// nombresContenido.push() nombresContenido + nombreAtleta
-// }
-// console.log (nombresContenido);
-// console.log (typeof nombresContenido)
-
-
+let atletas = data.athletes;
+let nombresContenido = []
+for (let i=0; i<atletas.length; i++){
+let nombreAtleta = atletas[i].name;
+nombresContenido.push(nombreAtleta)
+}
+function datoAleatorio(array) {
+return array[Math.floor(Math.random()*(nombresContenido.length))]
+}
+function datosAleatorios(numero, array) {
+    let atletasArray=[];
+     while(atletasArray.length<numero) {
+       let string = datoAleatorio(array);
+         if(atletasArray.indexOf(string) == -1) atletasArray.push(string);
+    }
+    return atletasArray;
+  }
+let pintarNombre = datosAleatorios(40, nombresContenido);
+pintarNombre.forEach(function(element){
+    let div = document.createElement("div");
+    div.textContent = element
+    document.getElementById("descripcionTarjeta").appendChild(div)
+})
 // evento menu responsive 
 let div = document.getElementById("menuToggle");
 div.addEventListener("click", mostrar)
 function mostrar() {
     let abrirNavegacion = document.getElementById("opcionesMenu");
     abrirNavegacion.classList.toggle("opcionesMenuOpen")
-
     let menuOpen = document.getElementById('menuToggle');
     menuOpen.classList.toggle('menuToggleOpen'); 
 }
-
 // Popup suscribirse y entrar
 let abrirPopup = document.getElementById ("suscribir"),
     overLay = document.getElementById ("overLay"),
     popUp = document.getElementById ("popUp"),
     btnCerrar = document.getElementById ("popUp");
-
     abrirPopup.addEventListener('click', function(){
         overLay.classList.add('active');
         popUp.classList.add('active');
-       
     });
     btnCerrar.addEventListener('click', function(e){
         e.preventDefault();
         overLay.classList.remove('active');
         popUp.classList.remove('active');
     });
-
 let abrirPopupEntrar = document.getElementById("entrar"),
      overLayEntrar = document.getElementById("overLayEntrar"),
      popUpEntrar = document.getElementById("popUpEntrar"),
      btnCerrarEntrar = document.getElementById("popUpEntrar");
-    
      abrirPopupEntrar.addEventListener("click", function(){
          overLayEntrar.classList.add("active");
          popUpEntrar.classList.add("active");
      });
-
      btnCerrarEntrar.addEventListener("click", function(e){
          e.preventDefault();
          overLayEntrar.classList.remove('active');
          popUpEntrar.classList.remove('active');
      });
-  
-
 // Desaparece y aparece la siguiente pagina 
-
-const btn1= document.getElementById("btn1")
+let btn1= document.getElementById("btn1")
 btn1.addEventListener("click", cambioDePagina);
 function cambioDePagina() {
     ocultarMostrar("paginaConoceMasAtletas","paginaPrincipal")
 }
-
 function ocultarMostrar(pantallaEncendida, pantallaApagada) {
     document.getElementById(pantallaApagada).style.display = "none";
     document.getElementById(pantallaEncendida).style.display = "inline";
 }
-
-// informacion atletas - 
