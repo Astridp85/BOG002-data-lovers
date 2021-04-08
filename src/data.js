b27cbff931090f5294a0505bd2ce6457e845e9f3
Vaneluuq <76055965+Vaneluuq@users.noreply.github.com>
2021-04-08 22:07:20 +0200

    Se añadieron filtros y boton refrescar

--- src/data.js
+++ src/data.js
@@ -3,18 +3,34 @@
 
 const filtroNombres = (array,atletaNombre) =>{
   return array.filter(atleta => atleta.name.toLowerCase().includes(atletaNombre.toLowerCase()))
-}
+};
 
 const filtroPais = (array, atletaPais) => {
   return array.filter(atleta => atleta.team === atletaPais)
-}
+};
 
 const paisesArray = (array) => {
 return array.map(obj => obj.team).filter((elemento, indice, arrayPais) => arrayPais.indexOf(elemento)==indice)
 };
 
+const filtroGenero = (array, atletaGenero) => {
+return array.filter(atleta => atleta.gender === atletaGenero)
+};
 
-const dataAtletas = (array,atletaNombre, atletaPais) => {
+const generoArray = (array) => {
+  return array.map(obj => obj.gender).filter((elemento, indice, arrayGenero) => arrayGenero.indexOf(elemento)==indice)
+};
+
+const filtroDeporte = (array, atletaDeporte) => {
+  return array.filter(atleta => atleta.sport === atletaDeporte )
+};
+
+const deporteArray = (array) => {
+  return array.map(obj =>obj.sport).filter((elemento, indice, arrayDeporte) => arrayDeporte.indexOf(elemento)==indice)
+};
+
+
+const dataAtletas = (array,atletaNombre, atletaPais, atletaGenero, atletaDeporte) => {
   if (atletaNombre !== '') {
     array = filtroNombres (array, atletaNombre);
     }
@@ -22,6 +38,15 @@ const dataAtletas = (array,atletaNombre, atletaPais) => {
   if (atletaPais !==''){
     array = filtroPais (array, atletaPais)
   }
+
+  if (atletaGenero !=='') {
+    array = filtroGenero (array, atletaGenero )
+  }
+
+  if (atletaDeporte !=='') {
+    array = filtroDeporte (array, atletaDeporte)
+  }
+
   
   const nuevoArray = array.map(obj => ({
     nombre: obj.name,
@@ -34,6 +59,6 @@ const dataAtletas = (array,atletaNombre, atletaPais) => {
   return nuevoArray;
 };
 
-export {dataAtletas, paisesArray}
+export {dataAtletas, paisesArray, generoArray, deporteArray}
 
  
--- src/main.js
+++ src/main.js
@@ -1,34 +1,61 @@
 import data from './data/athletes/athletes.js';
-import {dataAtletas, paisesArray} from './data.js';
+import {dataAtletas, paisesArray, generoArray, deporteArray} from './data.js';
 
 
 /* console.log (data.athletes);
 console.log(dataAtletas(data.athletes)); */ 
 /* DATOS ATLETAS */ 
 
+mostrarTarjetas(dataAtletas(data.athletes, "", "", "", ""));
+
 // filtros Página Conoce más de los atletas
+// filtro por deporte
 
-
-mostrarTarjetas(dataAtletas(data.athletes, ""));
-
-let mostrarPaises =  paisesArray(data.athletes)
-mostrarPaises.forEach(pais => {
-    let selectPais = document.getElementById("selectBusquedaPais")
-    let paises = document.createElement("option");
-    paises.text = pais
-    paises.value = pais
-    selectPais.appendChild(paises)
+let mostrarDeporte = deporteArray(data.athletes);
+mostrarDeporte.forEach(deporte => {
+  let selectDeporte = document.getElementById("selectBusquedaDeporte");
+  let deportes = document.createElement("option");
+  deportes.text = deporte;
+  deportes.value = deporte;
+  selectDeporte.appendChild(deportes);
 });
+
+// filtro por genero 
+
+let mostrarGenero = generoArray(data.athletes);
+mostrarGenero.forEach(genero => {
+ let selectGenero = document.getElementById("selectBusquedaGenero");
+ let generos = document.createElement("option");
+ generos.text = genero;
+ generos.value = genero;
+ selectGenero.appendChild(generos);
+});
+
+// filtro por pais
+
+let mostrarPaises =  paisesArray(data.athletes);
+mostrarPaises.forEach(pais => {
+    let selectPais = document.getElementById("selectBusquedaPais");
+    let paises = document.createElement("option");
+    paises.text = pais;
+    paises.value = pais;
+    selectPais.appendChild(paises);
+});
+
+// filtro por nombre
 
 const btnNombre = document.getElementById ("btnBuscar");
 btnNombre.addEventListener ('click', function(){
 let atletaNombre = document.getElementById("txtAtleta").value;
 let atletaPais = document.getElementById("selectBusquedaPais").value;
+let atletaGenero = document.getElementById ("selectBusquedaGenero").value;
+let atletaDeporte = document.getElementById("selectBusquedaDeporte").value;
 
-const filtroAtletas = dataAtletas(data.athletes,atletaNombre, atletaPais);
+const filtroAtletas = dataAtletas(data.athletes,atletaNombre, atletaPais, atletaGenero, atletaDeporte);
 mostrarTarjetas(filtroAtletas)
 });
 
+// funcion mostrar las tarjetas en pantalla
 
 function mostrarTarjetas(atletasArray){
     
@@ -81,6 +108,20 @@ atletasArray.forEach((atleta) => {
   medallaAtleta.appendChild(pintarMedalla);
   divCardAtleta.appendChild(medallaAtleta);
 });
+}
+
+// boton refrescar 
+
+let btnLimpiar = document.getElementById ("btnLimpiar");
+
+btnLimpiar.addEventListener("click", refrescar);
+function refrescar() {
+    document.getElementById("txtAtleta").value = "";
+    document.getElementById("selectBusquedaPais").value = "";
+    document.getElementById ("selectBusquedaGenero").value = "";
+    document.getElementById("selectBusquedaDeporte").value = "";
+
+    mostrarTarjetas(dataAtletas(data.athletes, "", "", "", ""));
 }
 
 
